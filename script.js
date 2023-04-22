a077403cbf6b67353babdeb7b04f72a6a746920d
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-04-22 15:46:32 +0100

    Level 100

--- script.js
+++ script.js
@@ -6,7 +6,33 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach(element => {
+    let card = document.createElement("div");
+    //Episode name 
+    const episodeName = document.createElement("h1")
+    episodeName.innerText = element.name
+    //Session and episode two digit number
+    const episodeSessionAndEppisode = document.createElement("h1")
+    episodeSessionAndEppisode.innerText = `S0${element.season}0${element.number}`
+    //Episode Image
+    const episodeImage = document.createElement("img")
+    episodeImage.src = element.image["medium"]
+    //Episode info
+    const episodeSummary = document.createElement("p");
+    episodeSummary.innerText = element.summary;
+    //creating a class for style
+    card.className = "card";
+    card.append(
+      episodeName,
+      episodeSessionAndEppisode,
+      episodeImage,
+      episodeSummary
+    );
+    rootElem.append(card)
+  });
+
 }
 
 window.onload = setup;
