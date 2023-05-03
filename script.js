5090da30d4fd95d37d0f859facc0d77914f3d835
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-05-03 17:47:20 +0100

    Live Search/200..

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  //makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  while (rootElem.hasChildNodes()) {
+    rootElem.removeChild(rootElem.lastChild);
+  }
 
-  episodeList.forEach(element => {
+
+  episodeList.forEach((element) => {
     let card = document.createElement("div");
-    //Episode name 
-    const episodeName = document.createElement("h1")
-    episodeName.innerText = element.name
+    //Episode name
+    const episodeName = document.createElement("h1");
+    episodeName.innerText = element.name;
     //Session and episode two digit number
-    const episodeSessionAndEppisode = document.createElement("h1")
-    episodeSessionAndEppisode.innerText = `S0${element.season}0${element.number}`
+    const episodeSessionAndEppisode = document.createElement("h1");
+    episodeSessionAndEppisode.innerText = `S0${element.season}0${element.number}`;
     //Episode Image
-    const episodeImage = document.createElement("img")
-    episodeImage.src = element.image["medium"]
+    const episodeImage = document.createElement("img");
+    episodeImage.src = element.image["medium"];
     //Episode info
     const episodeSummary = document.createElement("p");
-    episodeSummary.innerText = element.summary;
+    episodeSummary.innerHTML = element.summary;
     //creating a class for style
     card.className = "card";
     card.append(
@@ -30,9 +33,60 @@ function makePageForEpisodes(episodeList) {
       episodeImage,
       episodeSummary
     );
-    rootElem.append(card)
+    rootElem.append(card);
   });
-
 }
+
+ function filterEpisodes() {
+  const searchInput = document.querySelector("#search-bar");
+  const episodesArray = getAllEpisodes();
+
+  searchInput.addEventListener("input", function () {
+    let value = this.value;
+    let searchedEpisodes = [];
+    episodesArray.forEach(function (episode) {
+      if (
+        episode.name.toLowerCase().includes(value.toLowerCase()) == true ||
+        episode.summary.toLowerCase().includes(value.toLowerCase()) == true
+      ) {
+        searchedEpisodes.push(episode);
+      }
+    });
+    makePageForEpisodes(searchedEpisodes);
+  });
+}
+filterEpisodes(); 
+
+
+/* function filterEpisodes() {
+  const searchInput = document.querySelector("#search-bar");
+  const episodesArray = getAllEpisodes();
+  let searchedEpisodes = [];
+
+  searchInput.addEventListener("input", function () {
+    let value = this.value.trim(); // remove leading/trailing whitespace
+
+    if (value === "") {
+      // if search bar is empty, show all episodes
+      makePageForEpisodes(episodesArray);
+      return;
+    }
+
+    searchedEpisodes = episodesArray.filter(function (episode) {
+      return (
+        episode.name.toLowerCase().includes(value.toLowerCase()) ||
+        episode.summary.toLowerCase().includes(value.toLowerCase())
+      );
+    });
+    console.log(searchedEpisodes.length)
+    makePageForEpisodes(searchedEpisodes);
+  });
+} */
+
+//filterEpisodes();
+
+
+
+
 
 window.onload = setup;
